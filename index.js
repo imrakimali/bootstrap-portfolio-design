4e79072b1a14abc33deb0039a0584b39a10f3805
Rakim Ali <rakimali770@gmail.com>
2024-06-02 14:00:57 +0600

    typed js added

---
+++ index.js
@@ -0,0 +1,10 @@
+let typed = new Typed("#auto-type", {
+  strings: [
+    " Front-END Web Developer ",
+    "Responsive web Designer",
+    "SEO-EXPERT",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
